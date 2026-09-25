fbeacaf165ceeb893e2734232f222fc3f8f7b086
DANIA CANO LLAVE <dania.cano@ucb.edu.bo>
2026-09-25 08:58:30 -0400

    Agregar repositorio de comentarios

---
+++ SocialMedia.Core/Interfaces/ICommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SocialMedia.Core.Entities;
+
+namespace SocialMedia.Core.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> GetAllCommentsAsync();
+        Task<Comment> GetCommentByIdAsync(int id);
+        Task InsertComment(Comment comment);
+        Task UpdateComment(Comment comment);
+        Task DeleteComment(Comment comment);
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        public Task DeleteComment(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Comment>> GetAllCommentsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comment> GetCommentByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertComment(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateComment(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
