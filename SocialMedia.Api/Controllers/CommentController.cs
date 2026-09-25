b827537ff47744100226b6364e5f77ef7e2fd9c0
Benjamin Soto Cabellos <benjamin.soto@ucb.edu.bo>
2026-09-25 18:29:03 -0400

    Trabajo en Clase

---
+++ SocialMedia.Api/Controllers/CommentController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Repositories;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class commentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepository;
+        public commentController(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Getcomments()
+        {
+            var comments = await _commentRepository.GetAllCommentsAsync();
+            return Ok(comments);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetcommentById(int id)
+        {
+            var comment = await _commentRepository.GetCommentByIDAsync(id);
+            return Ok(comment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insertcomment(Comment newcomment)
+        {
+            await _commentRepository.InsertComment(newcomment);
+            return Created($"api/comment/{newcomment.Id}", newcomment);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Updatecomment(Comment comment)
+        {
+            await _commentRepository.UpdateComment(comment);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Deletecomment(Comment comment)
+        {
+            await _commentRepository.DeleteComment(comment);
+            return NoContent();
+        }
+    }
+}
--- SocialMedia.Infrastructure/obj/Debug/net10.0/SocialMedia.Infrastructure.AssemblyInfo.cs
+++ SocialMedia.Infrastructure/obj/Debug/net10.0/SocialMedia.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMedia.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f92846cfad48008a7a1b3670b221e45e2b095f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fff8029011ede319d8e3fd529f28ae5b92c4eab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMedia.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMedia.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
